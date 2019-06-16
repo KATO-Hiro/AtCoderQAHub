95f661132d145e4d132ab540af40ec48eaa334e5
Kato Hiroki <k.hiro1818@gmail.com>
2019-06-16 11:00:27 +0900

    Add pages and routing in home #115

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def about
+  end
+
+  def guideline
+  end
+
+  def userpage
+  end
+
+  def links
+  end
+
+  def login
+  end
 end
