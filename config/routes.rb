c3b24eb91904c7593dbb18d1578cd142dacae6d9
Kato Hiroki <k.hiro1818@gmail.com>
2019-08-31 16:36:31 +0900

    Fix format

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Rails.application.routes.draw do
   get "/problems"         , to: "problems#index"
   get "/problems/:task_id", to: "problems#show"
 
-  get "/problems/:task_id/questions"         , to: "questions#index"
-  get "/problems/:task_id/questions/:id"     , to: "questions#show"
-  get "/problems/:task_id/questions/new"     , to: "questions#new"
-  post "/problems/:task_id/questions/create" , to: "questions#create"
-  get "/problems/:task_id/questions/:id/edit", to: "questions#edit"
+  get "/problems/:task_id/questions"            , to: "questions#index"
+  get "/problems/:task_id/questions/:id"        , to: "questions#show"
+  get "/problems/:task_id/questions/new"        , to: "questions#new"
+  post "/problems/:task_id/questions/create"    , to: "questions#create"
+  get "/problems/:task_id/questions/:id/edit"   , to: "questions#edit"
   post "/problems/:task_id/questions/:id/update", to: "questions#update"
 end
