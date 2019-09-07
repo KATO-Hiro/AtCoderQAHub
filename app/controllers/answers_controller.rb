65fdff06d5291b26b7fe2b2ffa1d2db8a3b412b0
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-07 21:43:59 +0900

    Enable to create an answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -7,6 +7,20 @@ class AnswersController < ApplicationController
   end
 
   def create
+    task_id = params[:task_id]
+
+    @answer = Answer.new(
+      question_id: params[:question_id],
+      content: params[:content]
+    )
+
+    if @answer.save
+      flash[:notice] = "Submit your answer!"
+      redirect_to("/problems/#{task_id}/questions/#{@answer.question_id}")
+    # else
+      # 回答ページを表示
+      # render("")
+    end
   end
 
   def update
