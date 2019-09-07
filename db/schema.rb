d137c2246523f0737f15077cb8ca677cce5f8510
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-07 09:49:42 +0900

    Add model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_30_235740) do
+ActiveRecord::Schema.define(version: 2019_09_07_004738) do
+
+  create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "question_id"
+    t.string "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "problems", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "task_id"
