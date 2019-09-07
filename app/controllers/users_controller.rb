3bc73bad1f2bdee6fcf22a95671dff42a0fef5be
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-08 07:59:02 +0900

    Add todo list to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,47 @@
 class UsersController < ApplicationController
   def index
+    # DBからユーザー情報を取得
   end
 
   def show
+    # ユーザーidを取得
   end
 
   def new
+    # 新規作成
   end
 
   def create
+    # ユーザーidを取得
+    # 名前・パスワード・AtCoder IDを取得
+
+    # 保存
+    ## 成功
+    ### flashを表示
+    ### ユーザー一覧を表示
+    ## 失敗
+    ### 新規登録画面を表示
   end
 
   def edit
+    # ユーザーidを取得
   end
 
   def update
+    # ユーザーidを取得
+    # 名前・パスワード・AtCoder IDを取得
+
+    # 保存
+    ## 成功
+    ### flashを表示
+    ### ユーザー一覧を表示
+    ## 失敗
+    ### 編集画面を表示
   end
 
   def destroy
+    # ユーザーidを取得
+    # 削除
+    ## flashを表示
   end
 end
