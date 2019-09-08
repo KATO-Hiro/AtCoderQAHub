307097d53476ba33446b52c67c1eb98277a4074b
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-08 18:17:36 +0900

    Validate user_id

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
   validates :title, {presence: true, length: {maximum: 36}}
   validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
 end
