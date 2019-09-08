c868cc1ae6ecbe849298a775daa124edcac80182
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-08 16:01:42 +0900

    Enable to authenicate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   def set_current_user
     @current_user = User.find_by(id: session[:user_id])
   end
+
+  # ログインユーザーのみアクセスできるようにする
+  def authenticate_user
+    if @current_user == nil
+      flash[:notice] = "Login is needed."
+      redirect_to("/login")
+    end
+  end
 end
