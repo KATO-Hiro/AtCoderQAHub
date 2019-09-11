76299f0bdc5d91aa9e42d9d9c54450265df9568a
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-11 10:04:33 +0900

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
   validates :password, {presence: true, uniqueness: true}
-  act_as_voter
+  acts_as_voter
 end
