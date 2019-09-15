a0071ecb2d11683731d9f447f542844587646606
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-16 08:19:59 +0900

    Add tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   validates :name, {presence: true, uniqueness: true}
   validates :password, {presence: true, uniqueness: true}
   acts_as_voter
+
+  # TODO: ユーザーidから質問情報を取得できるようにする
+
+  # TODO: ユーザーidから回答情報を取得できるようにする
 end
