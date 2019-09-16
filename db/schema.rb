baadfcc993d662041583401c66870907e1112fea
Kato Hiroki <k.hiro1818@gmail.com>
2019-09-16 16:26:30 +0900

    Execute migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_002621) do
+ActiveRecord::Schema.define(version: 2019_09_16_072201) do
 
   create_table "answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "question_id"
@@ -40,10 +40,10 @@ ActiveRecord::Schema.define(version: 2019_09_11_002621) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.string "atcoder_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "votes", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
