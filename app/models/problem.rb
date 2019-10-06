c794f42a7c245faaf1c5f2a43262c26885ffab03
Kato Hiroki <k.hiro1818@gmail.com>
2019-10-06 18:36:35 +0900

    Add helper method

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,9 @@
 class Problem < ApplicationRecord
+  def duplicated?(target_problem)
+    if self.task_id == target_problem.task_id
+      return true
+    else
+      return false
+    end
+  end
 end
