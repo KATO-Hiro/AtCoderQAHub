9b8a5a8266681f5bd81f373ed61b1ae86b0a2827
Kato Hiroki <k.hiro1818@gmail.com>
2019-10-06 18:27:51 +0900

    Add helper method

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,4 +1,14 @@
 class Contest < ApplicationRecord
+  def held_on_the_same_day?
+    contests = Contest.where(start_epoch_second: self.start_epoch_second)
+
+    if contests.count >= 2
+      return true
+    else
+      return false
+    end
+  end
+
   def problems
     return Problem.where(contest_id: self.contest_id)
   end
