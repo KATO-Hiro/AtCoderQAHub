3eebd8c1df6de22d0aa8162333da7495fb5e8751
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-10 18:40:48 +0900

    Refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "バリデーション" do
     context "有効な状態" do
-      it "ハンドル名とパスワードがあれば有効な状態である" do
+      it "ハンドル名とパスワード（12文字以上）があれば有効な状態である" do
         user = User.new(
           name: "hiro_hiro",
           password: "hogefoobarfu",
@@ -13,35 +13,32 @@ RSpec.describe User, type: :model do
 
         expect(user).to be_valid
       end
-
-      it "パスワードが12文字以上なら有効な状態である" do
-        user = User.new(
-          name: "chokudai",
-          password: "hogefoobarfu",
-          atcoder_id: "",
-        )
-
-        expect(user).to be_valid
-      end
     end
 
     context "無効な状態" do
+      before do
+        @user = User.new()
+      end
+
       it "ハンドル名がなければ無効な状態である" do
-        user = User.new(name: nil)
-        user.valid?
-        expect(user.errors[:name]).to include("can't be blank")
+        @user.name = nil
+
+        @user.valid?
+        expect(@user.errors[:name]).to include("can't be blank")
       end
 
       it "空白スペース（1文字）のみのハンドル名は無効な状態である" do
-        user = User.new(name: " ")
-        user.valid?
-        expect(user.errors[:name]).to include("can't be blank")
+        @user.name = " "
+
+        @user.valid?
+        expect(@user.errors[:name]).to include("can't be blank")
       end
 
       it "空白スペース（2文字）のみのハンドル名は無効な状態である" do
-        user = User.new(name: "  ")
-        user.valid?
-        expect(user.errors[:name]).to include("can't be blank")
+        @user.name = " " * 2
+
+        @user.valid?
+        expect(@user.errors[:name]).to include("can't be blank")
       end
 
       it "重複したハンドル名なら無効な状態である" do
@@ -51,40 +48,33 @@ RSpec.describe User, type: :model do
           atcoder_id: "",
         )
 
-        user = User.new(
-          name: "chokudai",
-          password: "hogehogehoge",
-          atcoder_id: "",
-        )
+        @user.name = "chokudai"
+        @user.password = "hogehogehoge"
 
-        user.valid?
-        expect(user.errors[:name]).to include("has already been taken")
+        @user.valid?
+        expect(@user.errors[:name]).to include("has already been taken")
       end
 
       it "パスワードがなければ無効な状態である" do
-        user = User.new(password: nil)
-        user.valid?
-        expect(user.errors[:password]).to include("can't be blank")
+        @user.password = nil
+
+        @user.valid?
+        expect(@user.errors[:password]).to include("can't be blank")
       end
 
 
       it "空白のパスワードは無効な状態である" do
-        user = User.new(
-          password: " " * 12
-        )
+        @user.password = " " * 12
 
-        user.valid?
-        expect(user).to_not be_valid
+        @user.valid?
+        expect(@user).to_not be_valid
       end
 
       it "パスワードが11文字以下なら無効な状態である" do
-        user = User.new(
-          name: "chokudai",
-          password: "a" * 11,
-          atcoder_id: "",
-        )
+        @user.name = "chokudai"
+        @user.password = "a" * 11
 
-        expect(user).to_not be_valid
+        expect(@user).to_not be_valid
       end
 
       pending "重複したパスワードがある場合は無効な状態である" do
@@ -95,14 +85,11 @@ RSpec.describe User, type: :model do
           atcoder_id: "",
         )
 
-        user = User.new(
-          name: "aoki",
-          password: "takoyaki_daisuki",
-          password_confirmation: "takoyaki_daisuki",
-          atcoder_id: "",
-        )
+        @user.name = "aoki"
+        @user.password = "takoyaki_daisuki"
+        @user.password_confirmation = "takoyaki_daisuki"
 
-        expect(user.errors[:password_confirmation]).to include("has already been taken")
+        expect(@user.errors[:password_confirmation]).to include("has already been taken")
       end
 
       it "重複したAtCoder IDがある場合は無効な状態である" do
@@ -112,14 +99,12 @@ RSpec.describe User, type: :model do
           atcoder_id: "chokudai",
         )
 
-        user = User.new(
-          name: "aoki",
-          password: "hogehogehoge",
-          atcoder_id: "chokudai",
-        )
+        @user.name = "aoki"
+        @user.password = "hogehogehoge"
+        @user.atcoder_id = "chokudai"
 
-        user.valid?
-        expect(user.errors[:atcoder_id]).to include("has already been taken")
+        @user.valid?
+        expect(@user.errors[:atcoder_id]).to include("has already been taken")
       end
 
       it "AtCoder IDがAtCoderに登録されていない場合は無効な状態である"
