b514b86317c9d729b44df4a6119b540b72339160
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-10 10:23:15 +0900

    Enable to valid with name and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "ハンドル名とパスワードがあれば有効な状態である"
+  it "ハンドル名とパスワードがあれば有効な状態である" do
+    user = User.new(
+      name: "hiro_hiro",
+      password: "hogefoobarfu",
+      atcoder_id: "",
+    )
+
+    expect(user).to be_valid
+  end
+
   it "ハンドル名がなければ無効な状態である"
   it "重複したハンドル名なら無効な状態である"
   it "パスワードが12文字以上なら有効な状態である"
