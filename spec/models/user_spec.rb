b3e57b8610f06626ac2e690e58ada5c199c10cc9
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-10 10:28:12 +0900

    Enable to be invalid without name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,12 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it "ハンドル名がなければ無効な状態である"
+  it "ハンドル名がなければ無効な状態である" do
+    user = User.new(name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
   it "重複したハンドル名なら無効な状態である"
   it "パスワードが12文字以上なら有効な状態である"
   it "パスワードが11文字以下なら無効な状態である"
