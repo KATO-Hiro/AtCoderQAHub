4b3ac05d69c58494bb98e5440564ac31a3ba9237
Kato Hiroki <k.hiro1818@gmail.com>
2019-11-27 09:30:51 +0900

    Solve #398

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { "hiro_hiro" }
+    password { "hogefoobarfu" }
+    password_confirmation { "hogefoobarfu" }
+    atcoder_id { "" }
   end
 end
