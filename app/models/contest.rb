a3794dcdc23ae4109e2efaced876b608766a1f20
Kato Hiroki <k.hiro1818@gmail.com>
2019-12-04 17:56:18 +0900

    Remove old comments

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,21 +1,17 @@
 class Contest < ApplicationRecord
   class << self
-    # abc
     def abc
       return Contest.all.where("contest_id LIKE?", "abc%").order(start_epoch_second: :desc)
     end
 
-    # arc
     def arc
       return Contest.all.where("contest_id LIKE?", "arc%").order(start_epoch_second: :desc)
     end
 
-    # agc
     def agc
       return Contest.all.where("contest_id LIKE?", "agc%").order(start_epoch_second: :desc)
     end
 
-    # others
     def others
       return Contest.all.where.not("contest_id like? or contest_id like? or contest_id like?", "abc%", "arc%", "agc%").order(start_epoch_second: :desc)
     end
