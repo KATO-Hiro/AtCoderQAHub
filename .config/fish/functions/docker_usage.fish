7264b720c597afa256d5c2b6a647efe691e69432
Kato Hiroki <k.hiro1818@gmail.com>
2019-12-11 18:21:30 +0900

    Add reference

--- .config/fish/functions/docker_usage.fish
+++ .config/fish/functions/docker_usage.fish
@@ -1,4 +1,6 @@
 function docker_usage
+  # See:
+  # https://qiita.com/wakaba260/items/0a00c6c3aa7183a1cb99#%E3%83%A9%E3%83%83%E3%83%91%E3%83%BC%E3%82%B9%E3%82%AF%E3%83%AA%E3%83%97%E3%83%88%E3%82%92%E6%9B%B8%E3%81%8F
   echo "Commands:"
   echo "  init            Initialize image."
   echo "  build           Build or rebuild image."
